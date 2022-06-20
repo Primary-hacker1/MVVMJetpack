3ba54db7d15721fbff1df3910841ea92c395d8e7
zhangtao <zhangtao@2022>
2022-06-20 15:31:03 +0800

    网络不通，异常设置

--- common/src/main/java/com/common/throwe/ThrowableHandler.kt
+++ common/src/main/java/com/common/throwe/ThrowableHandler.kt
@@ -1,16 +1,28 @@
 package com.common.throwe
 
-import com.common.throwe.BaseResponseThrowable
-import java.lang.Exception
+import java.net.ConnectException
+
 
 /**
  * create by 2020/5/15
- * @author yx
+ * @author zt
  */
 object ThrowableHandler {
+
+    private var tag = ThrowableHandler::javaClass.name
+
     fun handleThrowable(e: Throwable): BaseResponseThrowable {
+
+        var message = e.message
+
+        when (e.cause) {
+            is ConnectException -> {
+                message = "网络异常或服务器异常！"
+            }
+        }
+
         e.printStackTrace()
         //这里自定义异常显比如说404或者后台返回的相关code
-        return BaseResponseThrowable(-1,e.message!!,e)
+        return BaseResponseThrowable(-1, message!!, e)
     }
 }
